b706891cc45692639e597914dd65e1fc4930e8e2
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-27 20:21:31 -0600

    Fix sql comments

--- data.sql
+++ data.sql
@@ -1,17 +1,17 @@
 /* Populate database with sample data. */
 
-// Insert animal Agumon
+/* Insert animal Agumon */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
 
-// Insert animal Gabumon
+/* Insert animal Gabumon */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
 
-// Insert animal Pikachu
+/* Insert animal Pikachu */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 
-// Insert animal Devimon
+/* Insert animal Devimon */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
