f0c1b1720b26848f91635731441629f977a6a9f0
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-28 16:23:42 -0600

    Create more quieries using aggregate functions and more

--- queries.sql
+++ queries.sql
@@ -26,3 +26,109 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 /* it can also be done as shown: */
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.3 AND 17.4;
+
+
+
+/*
+1- Inside a transaction update the animals table by setting the species column to unspecified
+*/
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+/* 
+2- Verify that change was made
+*/
+SELECT * FROM animals;
+/* 
+3- Then roll back the change and verify that the species columns went back to the state before the transaction.
+*/
+ROLLBACK TRANSACTION;
+
+
+
+/* 
+Inside a transaction:
+Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+*/
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+/* 
+Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+*/
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL OR species = '';
+/* 
+Commit the transaction.
+*/
+COMMIT;
+/*
+Verify that change was made and persists after commit.
+*/
+SELECT * FROM animals;
+ROLLBACK; /* This won't work */
+
+
+
+/* Inside a transaction delete all records in the animals table */
+BEGIN;
+DELETE FROM animals;
+/* verify all are deleted */
+SELECT * FROM animals;
+/* roll back the transaction */
+ROLLBACK;
+
+
+
+/* 
+Inside a transaction:
+Delete all animals born after Jan 1st, 2022
+Check animals were deleted
+*/
+BEGIN; DELETE FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) >= 2022 AND EXTRACT(MONTH FROM date_of_birth) >= 1 AND EXTRACT(DAY FROM date_of_birth) >= 1;
+SELECT * FROM animals;
+/* 
+Create a savepoint for the transaction
+Update all animals' weight to be their weight multiplied by -1
+Check animals weight was updated
+*/
+SAVEPOINT BEFORE_WEIGHT_UPDATE; UPDATE animals SET weight_kg = weight_kg * -1;
+SELECT * FROM animals;
+/* 
+Rollback to the savepoint
+Check animals weight is back to the initial value
+*/
+ROLLBACK TO BEFORE_WEIGHT_UPDATE;
+SELECT * FROM animals;
+/* 
+Update all animals' weights that are negative to be their weight multiplied by -1.
+Commit transaction
+*/
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+
+
+/* 
+Write queries to answer the following questions:
+- How many animals are there?
+*/
+SELECT COUNT(*) FROM animals;
+
+/* How many animals have never tried to escape? */
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/* What is the average weight of animals? */
+SELECT AVG(weight_kg) FROM animals;
+
+/* Who escapes the most, neutered or not neutered animals? */
+SELECT neutered, SUM(escape_attempts) as total_attempts FROM animals GROUP BY neutered ORDER BY SUM(escape_attempts) DESC limit 1;
+
+/* 
+What is the minimum and maximum weight of each type of animal?
+*/
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species; /* by species */
+SELECT neutered, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY neutered; /* by neutered */
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals; /* min and max from all animals */
+
+/* 
+What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+*/
+SELECT AVG(escape_attempts) FROM animals WHERE EXTRACT(YEAR FROM date_of_birth)BETWEEN 1990 AND 2000 GROUP BY species;
