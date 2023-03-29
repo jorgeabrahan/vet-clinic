7094eee3445638c556ef71cc2d0db69aa1ccecc4
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-29 15:48:23 -0600

    Insert data into the owners and species table

--- data.sql
+++ data.sql
@@ -37,3 +37,22 @@ VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+
+/* Insert data into the owners table */
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+/* Insert data into the species table */
+INSERT INTO species (name) VALUES ('Pokemon');
+
+INSERT INTO species (name) VALUES ('Digimon');
