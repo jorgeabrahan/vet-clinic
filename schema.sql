c1f58e364e1bd449dc5f337f2f246b939da734f7
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-29 15:43:31 -0600

    Add columns as foreign key references to the animals table

--- schema.sql
+++ schema.sql
@@ -34,3 +34,18 @@ CREATE TABLE species(
 	name VARCHAR(250),
 	PRIMARY KEY(id)
 );
+
+/* Modify animals table:
+- Remove column species
+*/
+ALTER TABLE animals DROP COLUMN species;
+/*
+- Add column species_id which is a foreign key referencing species table
+*/
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+/* 
+- Add column owner_id which is a foreign key referencing the owners table
+*/
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
